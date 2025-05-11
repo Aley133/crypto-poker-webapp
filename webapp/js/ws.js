cfef1b4738ad1b40e6cb2bf89f735e7a05cc9e74
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-05-11 11:47:45 +0300

    Update ws.js

--- webapp/js/ws.js
+++ webapp/js/ws.js
@@ -1,1 +1,23 @@
+// js/ws.js
+export class GameSocket {
+  constructor(tableId, onState) {
+    const proto = location.protocol === 'https:' ? 'wss' : 'ws';
+    this.ws = new WebSocket(`${proto}://${location.host}/ws/game/${tableId}`);
+    this.ws.onmessage = e => {
+      try { onState(JSON.parse(e.data)); }
+      catch (err) { console.error('Invalid WS JSON', err); }
+    };
+    this.ws.onclose = () => setTimeout(() => this.reconnect(onState), 3000);
+  }
 
+  reconnect(onState) {
+    console.log('Reconnecting WS…');
+    this.constructor(this.tableId, onState);
+  }
+
+  send(action, amount = 0) {
+    if (this.ws.readyState === WebSocket.OPEN) {
+      this.ws.send(JSON.stringify({ user_id: this.userId, action, amount }));
+    }
+  }
+}
