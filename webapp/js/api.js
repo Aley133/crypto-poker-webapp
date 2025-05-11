ebcd4baa2df07b4124f4797e950bfbbf0ef998bb
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-05-11 12:09:48 +0300

    Update api.js

--- webapp/js/api.js
+++ webapp/js/api.js
@@ -1,7 +1,7 @@
-// js/api.js
-export const api = async (path, params = {}) => {
+// webapp/js/api.js
+export async function api(path, params = {}) {
   const q = new URLSearchParams(params);
-  const res = await fetch(`${path}?${q}`, { credentials: 'same-origin' });
+  const res = await fetch(`${path}?${q}`, { credentials:'same-origin' });
   if (!res.ok) throw await res.json();
   return res.json();
-};
+}
