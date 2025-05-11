1f8175500ed8a1cabb8e317f4fb246fdfdf04612
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-05-11 20:23:53 +0300

    Update ws.js

--- webapp/js/ws.js
+++ webapp/js/ws.js
@@ -1,21 +1,18 @@
 // webapp/js/ws.js
 export class GameSocket {
-  constructor(tableId, userId, onState) {
+  constructor(tableId, userId, onMessage) {
     this.userId = userId;
     const proto = location.protocol === 'https:' ? 'wss' : 'ws';
     this.ws = new WebSocket(`${proto}://${location.host}/ws/game/${tableId}`);
-    this.ws.onmessage = e => {
-      try { onState(JSON.parse(e.data)); }
-      catch { console.error('Invalid WS JSON', e.data); }
-    };
-    this.ws.onclose = () => setTimeout(() => this.reconnect(onState), 3000);
+    this.ws.onmessage = e => onMessage(JSON.parse(e.data));
+    this.ws.onclose = () => setTimeout(() => this.reconnect(onMessage), 3000);
   }
-  reconnect(onState) {
-    new GameSocket(this.tableId, this.userId, onState);
+  reconnect(onMessage) {
+    new GameSocket(this.tableId, this.userId, onMessage);
   }
-  send(action, amount = 0) {
-    if (this.ws.readyState === WebSocket.OPEN) {
-      this.ws.send(JSON.stringify({ user_id: this.userId, action, amount }));
+  send(action, amount=0) {
+    if (this.ws.readyState===WebSocket.OPEN) {
+      this.ws.send(JSON.stringify({ user_id:this.userId, action, amount }));
     }
   }
 }
