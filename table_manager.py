e2d563ea411c06cdcdbe9b710d4b8702e7749de3
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-06-19 23:17:17 +0300

    Update table_manager.py

--- table_manager.py
+++ table_manager.py
@@ -43,6 +43,7 @@ class TableManager:
             # Если стало меньше MIN_PLAYERS — сбрасываем флаг started
             if len(players) < 2:
                 state["started"] = False
+                 state["phase"] = "waiting"  # <== ДОБАВИТЬ ЭТО
 
         # HTTP-логика: seat_map и БД
         tables.leave_table(table_id, player_id)
